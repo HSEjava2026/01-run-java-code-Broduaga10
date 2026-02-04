2d80731106e2a53db527cc0cdcac9bfb5170b7cf
Broduaga <95465458+Broduaga10@users.noreply.github.com>
2026-02-04 20:55:44 +0300

    Выполнил работу, 4 уровня
    # Conflicts:
    #	README.md

---
+++ src/ru/hse/lab1/Hello.java
@@ -0,0 +1,12 @@
+package ru.hse.lab1;
+
+/**
+ * @author Барсуков Михаил Сергеевич
+ * @version 1.0
+ * @since 2026
+ */
+public class Hello {
+    static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
